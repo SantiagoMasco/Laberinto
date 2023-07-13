3036d5aa4296e0b4c3e88e64caa0fa9a0dcc4cfa
santiago masco <santiimasco@gmail.com>
2023-07-13 02:44:15 -0300

    funciones

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pygame
 
+global_x = 1
+global_y = 0
 
-def drawMaze(maze, screen, screen_width, screen_height):
+def draw_maze(maze, screen, screen_width, screen_height):
     block_size = 25
-
-
 
     for row in range(len(maze)):
         for col in range(len(maze[row])):
@@ -17,57 +17,58 @@ def drawMaze(maze, screen, screen_width, screen_height):
             else:
                 pygame.draw.rect(screen, (0, 0, 0), (col * block_size, row * block_size, block_size, block_size))
 
-    #actualiza
     pygame.display.flip()
 
 
-def startGame():
+def start_game():
+    global global_x, global_y
+
     screen_width = 800
     screen_height = 600
 
     pygame.init()
     screen = pygame.display.set_mode((screen_width, screen_height))
-
     pygame.display.set_caption("maze")
 
     with open("maze.txt", "r") as file:
         maze = [[int(cell) for cell in line.strip().split(",")] for line in file if line.strip()]
-        x = 1
-        y = 0
 
     running = True
     while running:
-        maze[y][x] = 2
+        maze[global_y][global_x] = 2
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_w or event.key == pygame.K_UP:
-                    if(y > 0):
-                        if(maze[y-1][x] != 0):
-                            maze[y][x] = 1
-                            y -= 1
-                            maze[y][x] = 2
+                    if global_y > 0:
+                        if maze[global_y - 1][global_x] != 0:
+                            maze[global_y][global_x] = 1
+                            global_y -= 1
+                            maze[global_y][global_x] = 2
                 if event.key == pygame.K_s or event.key == pygame.K_DOWN:
-                    if(maze[y+1][x] != 0):
-                        maze[y][x] = 1
-                        y += 1
-                        maze[y][x] = 2
+                    if global_y < len(maze) - 1:
+                        if maze[global_y + 1][global_x] != 0:
+                            maze[global_y][global_x] = 1
+                            global_y += 1
+                            maze[global_y][global_x] = 2
                 if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                    if (maze[y][x + 1] != 0):
-                        maze[y][x] = 1
-                        x += 1
-                        maze[y][x] = 2
+                    if global_x < len(maze[0]) - 1:
+                        if maze[global_y][global_x + 1] != 0:
+                            maze[global_y][global_x] = 1
+                            global_x += 1
+                            maze[global_y][global_x] = 2
                 if event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                    if (maze[y][x - 1] != 0):
-                        maze[y][x] = 1
-                        x -= 1
-                        maze[y][x] = 2
-        drawMaze(maze, screen, screen_width, screen_height)
-        if(maze[19][28] == 2):
+                    if global_x > 0:
+                        if maze[global_y][global_x - 1] != 0:
+                            maze[global_y][global_x] = 1
+                            global_x -= 1
+                            maze[global_y][global_x] = 2
+        draw_maze(maze, screen, screen_width, screen_height)
+        if maze[19][28] == 2:
             running = False
 
     pygame.quit()
 
 
-startGame()
+start_game()
